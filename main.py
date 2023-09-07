7f523284945ac53b115721c4b834eae7d3523a2f
shrnshlm <shrnshlm@gmail.com>
2023-09-07 11:32:45 +0300

    1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from flask import Flask
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+app = Flask(__name__)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,4 +16,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+    app.run(host='0.0.0.0', port=8080)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
